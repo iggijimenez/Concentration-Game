61526a03907833b9d9a26249d262d8f5f35ce9f7
Iggi <iggijimenez@gmail.com>
2021-09-05 19:15:00 -0700

    Done with the work

--- Stanford-Concentration/ViewController.swift
+++ Stanford-Concentration/ViewController.swift
@@ -8,22 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    
-    @IBAction func touchCard(_ sender: UIButton) {
-        flipCard(withEmoji: "👻", on: sender)
-    }
-    
-    func flipCard(withEmoji emoji: String, on button: UIButton ){
-        if button.currentTitle == emoji {
-            print("card is being flipped")
-            button.setTitle("", for: .normal)
-            button.backgroundColor = .orange
-        } else {
-            print("card is number 2")
-            button.setTitle(emoji, for: .normal)
-            button.backgroundColor = .white
+    var flipCount = 0 {
+        didSet{
+            flipCountLabel.text = "Flips: \(flipCount)"
         }
     }
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices = ["👻","🎃","🎃","👻"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("Card is not in CardButtons")
+        }
+        
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = UIColor.orange
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = UIColor.white
+        }
+    }
+    
 }
 
